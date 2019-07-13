ae5cf1dc9776c8ec2d1cb9c2221507f10377321f
Alexander Sheveryaev <alexsv.working@gmail.com>
2019-07-13 08:28:21 +0300

    View bild activiti all components

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("M_MainActiviti","onCreate")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("M_MainActiviti","onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("M_MainActiviti","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("M_MainActiviti","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("M_MainActiviti","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("M_MainActiviti","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("M_MainActiviti","onDestroy")
     }
 }
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -1,4 +1,21 @@
 package ru.skillbranch.devintensive.models
 
+import android.graphics.Color
+
 class Bender {
 }
+
+//enum class Status(val color: Triple) {
+//    NORMAL(Triple(255, 255, 255)) ,
+//    WARNING(Triple(255, 120, 0)),
+//    DANGER(Triple(255, 60, 60)),
+//    CRITICAL(Triple(255, 0, 0)) ;
+//}
+//enum class Question(val question: String, val answers: List) {
+//    NAME("Как меня зовут?", listOf("Бендер", "bender"))
+//    PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender"))
+//    MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood"))
+//    BDAY("Когда меня создали?", listOf("2993"))
+//    SERIAL("Мой серийный номер?", listOf("2716057"))
+//    IDLE("На этом все, вопросов больше нет", listOf())
+//}
